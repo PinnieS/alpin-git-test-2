31b381b0a0c7d910654f143a471c1a2715348ed2
Alvin Rig <alvinrigz@gmail.com>
2024-01-31 18:00:56 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remote</title>
 </head>
+
 <body>
     <h1>Alpen!!!</h1>
 
@@ -12,6 +14,18 @@
     echo "hello world"
         ?>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sit debitis aut natus reprehenderit esse non optio, consequuntur rerum, doloremque facere officia error omnis! Adipisci perspiciatis nemo enim porro. Mollitia, dolorum quasi vero in maxime ipsam, dolor sint quaerat labore nulla eligendi ea quas pariatur non illo minima, tempora adipisci? Tempora labore sunt dolorem adipisci, dolore ex aspernatur temporibus totam corporis sed necessitatibus vero alias consequuntur eligendi repellendus hic unde amet, qui officiis et quod. Cum maiores ipsum eligendi culpa nemo? Modi minus expedita illo aspernatur inventore cumque autem consequatur, iusto repellat nulla accusamus non debitis mollitia esse nostrum fugiat.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sit debitis aut natus reprehenderit esse non
+        optio, consequuntur rerum, doloremque facere officia error omnis! Adipisci perspiciatis nemo enim porro.
+        Mollitia, dolorum quasi vero in maxime ipsam, dolor sint quaerat labore nulla eligendi ea quas pariatur non illo
+        minima, tempora adipisci? Tempora labore sunt dolorem adipisci, dolore ex aspernatur temporibus totam corporis
+        sed necessitatibus vero alias consequuntur eligendi repellendus hic unde amet, qui officiis et quod. Cum maiores
+        ipsum eligendi culpa nemo? Modi minus expedita illo aspernatur inventore cumque autem consequatur, iusto
+        repellat nulla accusamus non debitis mollitia esse nostrum fugiat.</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
+
 </html>

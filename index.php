b3efd00673ca2a6477c04bb44da5719f770859f3
Alvin Rig <alvinrigz@gmail.com>
2024-01-31 17:55:57 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -11,5 +11,7 @@
     <?php
     echo "hello world"
         ?>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sit debitis aut natus reprehenderit esse non optio, consequuntur rerum, doloremque facere officia error omnis! Adipisci perspiciatis nemo enim porro. Mollitia, dolorum quasi vero in maxime ipsam, dolor sint quaerat labore nulla eligendi ea quas pariatur non illo minima, tempora adipisci? Tempora labore sunt dolorem adipisci, dolore ex aspernatur temporibus totam corporis sed necessitatibus vero alias consequuntur eligendi repellendus hic unde amet, qui officiis et quod. Cum maiores ipsum eligendi culpa nemo? Modi minus expedita illo aspernatur inventore cumque autem consequatur, iusto repellat nulla accusamus non debitis mollitia esse nostrum fugiat.</p>
 </body>
 </html>
